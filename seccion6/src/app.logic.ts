1d4387cea90c12e1d741469ac232e228553c8ac1
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2024-07-18 03:43:53 -0600

    seccion6 completada

---
+++ seccion6/src/app.logic.ts
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import { yarg } from './config/plugins/args.plugin';
+
+const { b:base, l:limit, s:showTable } = yarg;
+
+let outputMessage = '';
+const headerMessage = `
+==================================
+       Tabla del ${base}
+==================================\n
+`;
+
+for (let index = 0; index <= limit; index++) {
+    outputMessage += `${base} x ${index} = ${base * index}\n`;
+}
+
+outputMessage = headerMessage + outputMessage;
+if(showTable){
+    console.log(outputMessage);
+}
+
+const outputPath = `outputs`;
+fs.mkdirSync(outputPath, { recursive: true });
+fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, outputMessage);
+console.log('File created!');
