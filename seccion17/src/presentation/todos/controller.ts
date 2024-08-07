a5425e5707f08b6c8bc5a62ba4dbd7b08bef7280
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2024-08-06 19:12:09 -0600

    seccion17 completada

--- seccion17/src/presentation/todos/controller.ts
+++ seccion17/src/presentation/todos/controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { prisma } from "../../data/postgres";
 import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos";
 import { error } from "console";
-import { CreateTodo, DeleteTodo, GetTodo, GetTodos, TodoRepository, UpdateTodo } from "../../domain";
+import { CreateTodo, CustomError, DeleteTodo, GetTodo, GetTodos, TodoRepository, UpdateTodo } from "../../domain";
 
 export class TodoController {
     //* DI
@@ -10,10 +10,18 @@ export class TodoController {
         private readonly todoRepository : TodoRepository
     ){}
 
+    private handleError = (res : Response, error : unknown) => {
+        if(error instanceof CustomError){
+            return res.status(error.statusCode).json({ error: error.message });
+        }
+        
+        return res.status(500).json({ error: 'Internal server error - check logs' });
+    }
+
     public getTodos = (req : Request, res: Response) => {
        new GetTodos(this.todoRepository).execute()
         .then(todos => res.json(todos))
-        .catch(error => res.status(400).json({ error }))
+        .catch(error => this.handleError(res, error));
     }
 
     public getTodoById = (req : Request, res : Response ) => {
@@ -21,16 +29,16 @@ export class TodoController {
 
         new GetTodo(this.todoRepository).execute(id)
         .then(todos => res.json(todos))
-        .catch(error => res.status(400).json({ error }))
+        .catch(error => this.handleError(res, error));
     };
 
     public createTodo = (req : Request, res: Response) => {
         const [error, createTodoDto] = CreateTodoDto.create(req.body);
         if(error) return res.status(400).json({ error });
-        
+    
         new CreateTodo(this.todoRepository).execute(createTodoDto!)
-        .then(todos => res.json(todos))
-        .catch(error => res.status(400).json({ error }))
+        .then(todos => res.status(201).json(todos))
+        .catch(error => this.handleError(res, error));
     }
 
     public updateTodo = (req : Request, res: Response) => {
@@ -40,7 +48,7 @@ export class TodoController {
 
         new UpdateTodo(this.todoRepository).execute(updateTodoDto!)
         .then(todos => res.json(todos))
-        .catch(error => res.status(400).json({ error }))
+        .catch(error => this.handleError(res, error));
     }
 
     public deleteTodo = (req : Request, res: Response) => {
@@ -48,6 +56,6 @@ export class TodoController {
       
         new DeleteTodo(this.todoRepository).execute(id)
         .then(todos => res.json(todos))
-        .catch(error => res.status(400).json({ error }))
+        .catch(error => this.handleError(res, error));
     }
 }
--- seccion17/tests/presentation/todos/routes.test.ts
+++ seccion17/tests/presentation/todos/routes.test.ts
@@ -11,11 +11,14 @@ describe('Todo route testing', () => {
         testServer.close();
     });
 
+    beforeEach(async() => {
+        await prisma.todo.deleteMany();
+    });
+
     const todo1 = { text: 'Hola Mundo 1' };
     const todo2 = { text: 'Hola Mundo 2' };
 
     test('should return TODOs api/todos', async() => {  
-        await prisma.todo.deleteMany();
         await prisma.todo.createMany({
             data: [todo1, todo2]
         });
@@ -29,5 +32,141 @@ describe('Todo route testing', () => {
         expect(body[0].text).toBe(todo1.text);
         expect(body[1].text).toBe(todo2.text);
         expect(body[0].completedAt).toBeNull();
-    })
+    });
+
+    test('should return a TODO api/todos/:id', async() => {  
+        const todo = await prisma.todo.create({
+            data: todo1
+        });
+
+        const { body } = await request(testServer.app)
+            .get(`/api/todos/${todo.id}`)
+            .expect(200)
+        
+        expect(body).toEqual({
+            id: todo.id,
+            text: todo.text,
+            completedAt: todo.completedAt,
+        });
+    });
+
+    test('should return a 404 NotFound api/todos/:id', async() => {  
+        const todoID = 999;
+
+        const { body } = await request(testServer.app)
+            .get(`/api/todos/${todoID}`)
+            .expect(404)
+        
+        expect(body).toEqual({
+            error: `Todo with id ${todoID} not found`
+        });
+    });
+
+    test('should return a new Todo api/todos', async() => {  
+        const { body } = await request(testServer.app)
+            .post('/api/todos')
+            .send(todo1)
+            .expect(201)
+        
+        expect(body).toEqual({
+            id: expect.any(Number),
+            text: todo1.text,
+            completedAt: null
+        });
+    });
+
+    test('should return an error if text is present api/todos', async() => {  
+        const { body } = await request(testServer.app)
+            .post('/api/todos')
+            .send({})
+            .expect(400)
+        
+        expect(body).toEqual({
+            error: 'Text property is required' 
+        });
+    });
+
+    test('should return an error if text is empty api/todos', async() => {  
+        const { body } = await request(testServer.app)
+            .post('/api/todos')
+            .send({ text: '' })
+            .expect(400)
+        
+        expect(body).toEqual({
+            error: 'Text property is required' 
+        });
+    });
+
+    test('should return an updated TODO api/todos/:id', async() => {  
+        const todo = await prisma.todo.create({
+            data: todo1
+        });
+
+        const { body } = await request(testServer.app)
+            .put(`/api/todos/${todo.id}`)
+            .send({ text: 'Hola mundo UPDATE', completedAt: '2024-08-06' })
+            .expect(200)
+        
+        expect(body).toEqual({
+            id: expect.any(Number),
+            text: 'Hola mundo UPDATE',
+            completedAt: '2024-08-06T00:00:00.000Z',
+        });
+    });
+
+    test('should return 404 if TODO not found when updating', async() => {  
+        const todoID = 999;
+        const { body } = await request(testServer.app)
+            .put(`/api/todos/${todoID}`)
+            .send({ text: 'Hola mundo UPDATE', completedAt: '2024-08-06' })
+            .expect(404)
+        
+        expect(body).toEqual({
+            error: `Todo with id ${todoID} not found` 
+        });
+    });
+
+    test('should return an updated TODO only the date', async() => {  
+        const todo = await prisma.todo.create({
+            data: todo1
+        });
+
+        const { body } = await request(testServer.app)
+            .put(`/api/todos/${todo.id}`)
+            .send({ completedAt: '2024-09-06' })
+            .expect(200)
+        
+        expect(body).toEqual({
+            id: expect.any(Number),
+            text: todo1.text,
+            completedAt: '2024-09-06T00:00:00.000Z',
+        });
+    });
+
+    test('should delete a TODO api/todos/:id', async() => {  
+        const todo = await prisma.todo.create({
+            data: todo1
+        });
+
+        const { body } = await request(testServer.app)
+            .delete(`/api/todos/${todo.id}`)
+            .expect(200)
+        
+        expect(body).toEqual({
+            id: expect.any(Number),
+            text: todo1.text,
+            completedAt: null,
+        });
+    });
+
+    test('should return 404 if todo do not exist delete api/todos/:id', async() => {  
+        const todoID = 999;
+        const { body } = await request(testServer.app)
+            .delete(`/api/todos/${todoID}`)
+            .expect(404)
+        
+        expect(body).toEqual({
+            error: `Todo with id ${todoID} not found` 
+        });
+    });
 })
