7c9871b0f1ee3512b1149acb522d254249636695
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2024-08-12 21:54:40 -0600

    seccion20 avance

---
+++ seccion20/src/presentation/category/controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { CreateCategoryDto, CustomError } from "../../domain";
+
+export class CategoryController {
+    constructor(){}
+
+    private handleError = (res : Response, error : unknown) => {
+        if(error instanceof CustomError){
+            return res.status(error.statusCode).json({ error: error.message });
+        }
+        
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+
+    public createCategory = (req : Request, res : Response) => {
+        const [error, createCategoryDto] = CreateCategoryDto.create(req.body);
+        if(error) return res.status(400).json({error});
+        res.json('createCategory');
+    }
+
+    public getCategories = (req : Request, res : Response) => {
+        res.json('getCategories');
+    }
+}
